b53e1d64b4ad41b8122ba81748523eb1322b6d2b
xiaojun.liu <xiaojun.liu@work100.net>
2020-02-02 12:30:32 +0800

    初始化

---
+++ src/main/java/net/work100/training/stage1/basic/BasicSyntax.java
@@ -0,0 +1,19 @@
+package net.work100.training.stage1.basic;
+
+/**
+ * <p>Title: BasicSyntax</p>
+ * <p>Description: </p>
+ *
+ * @author liuxiaojun
+ * @date 2020-02-02 12:23
+ * ------------------- History -------------------
+ * <date>      <author>       <desc>
+ * 2020-02-02   liuxiaojun     初始创建
+ * -----------------------------------------------
+ */
+public class BasicSyntax {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/net/work100/training/stage1/basic/HellWorld.java
@@ -0,0 +1,25 @@
+package net.work100.training.stage1.basic;
+
+/**
+ * <p>Title: HellWorld</p>
+ * <p>Description: </p>
+ *
+ * @author liuxiaojun
+ * @date 2020-02-02 12:23
+ * ------------------- History -------------------
+ * <date>      <author>       <desc>
+ * 2020-02-02   liuxiaojun     初始创建
+ * -----------------------------------------------
+ */
+public class HellWorld {
+
+    /**
+     * 第一个Java应用程序
+     * 打印字符串：Hello World!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
